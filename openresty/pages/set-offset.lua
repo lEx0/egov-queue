0d10785cd24618918e88f242ca22f2c11ec87c98
Anton Sergeyev <sergeyev@kolesa.kz>
2020-04-05 13:05:35 +0600

    set-offset

---
+++ openresty/pages/set-offset.lua
@@ -0,0 +1,27 @@
+local redis = require "resty.redis"
+local red = redis:new()
+
+red:set_timeouts(1000, 1000, 1000) -- 1 sec
+
+local ok, err = red:connect("redis", 6379)
+if not ok then
+    ngx.say("failed to connect to redis: ", err)
+    return
+end
+
+red:set_timeouts(1000, 1000, 1000) -- 1 sec
+
+local ok, err = red:connect("redis", 6379)
+if not ok then
+    ngx.say("failed to connect to redis: ", err)
+    return
+end
+
+local args, err = ngx.req.get_uri_args()
+
+if not args.offset then
+    ngx.say("Usage: ?offset=123")
+else
+    red:set("global_offset", args.offset)
+    ngx.say("ok, new offset is ", args.offset)
+end
