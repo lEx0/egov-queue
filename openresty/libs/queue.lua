6b67c69489bccc25b33c54030918b82edfb64b69
Amangeldy Kadyl <kadyl@kolesa.kz>
2020-04-05 15:46:01 +0600

    global consts

--- openresty/libs/queue.lua
+++ openresty/libs/queue.lua
@@ -8,7 +8,7 @@ local _M = {
     _VERSION = '0.0.1'
 }
 
-function _M.getPosition(size)
+function _M.getPosition()
     local ck = require "resty.cookie"
     local cookie, err = ck:new()
     if not cookie then
@@ -19,20 +19,20 @@ function _M.getPosition(size)
     local redis = require "resty.redis"
     local red = redis:new()
 
-    red:set_timeouts(1000, 1000, 1000) -- 1 sec
+    red:set_timeouts(ngx.var.queue_redis_timeout, ngx.var.queue_redis_timeout, ngx.var.queue_redis_timeout) -- 1 sec
 
-    local ok, err = red:connect("redis", 6379)
+    local ok, err = red:connect(ngx.var.queue_redis_host, ngx.var.queue_redis_port)
     if not ok then
         return nil, err
     end
 
     local uuidGenerator = require "resty.jit-uuid"
     local currentUuid
-    local uuidCookie, err = cookie:get("uuid")
+    local uuidCookie, err = cookie:get(ngx.var.queue_cookie_name)
     if not uuidCookie then
         currentUuid = uuidGenerator()
         local ok, err = cookie:set({
-            key = "uuid",
+            key = ngx.var.queue_cookie_name,
             value = currentUuid,
             samesite = "Strict"
         })
@@ -45,16 +45,16 @@ function _M.getPosition(size)
         myPosition, err = red:incr("global_counter")
     end
 
-    -- время жизни юзера - 20 секунд
-    red:setex(currentUuid, 20, myPosition)
+    -- время жизни юзера
+    red:setex(currentUuid, tonumber(ngx.var.queue_session_lifetime), myPosition)
 
     myPosition=tonumber(myPosition)
     ngx.log(ngx.INFO, "client position: " .. myPosition)
 
     local globalOffset, err = red:get("global_offset")
     if globalOffset == ngx.null then
-        globalOffset = tostring(size)
-        red:set("global_offset", tostring(size))
+        globalOffset = tostring(ngx.var.queue_max_sessions)
+        red:set("global_offset", tostring(ngx.var.queue_max_sessions))
     end
 
     return myPosition - globalOffset, nil
